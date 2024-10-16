5f2954c06f2aecde9cc92abf490dd54431bdce12
abdulrehmanQok <abdulrehmanmehmood282@gmail.com>
2024-10-16 18:09:52 +0500

    python basics

---
+++ project.py
@@ -0,0 +1,11 @@
+print("python basics")
+str1="this is me"
+age=21
+weight=75.55
+#number strings liost tuplle dictionaries
+print("3+5 =",3+4)
+
+mls='''this 
+is a 
+multi line string '''
+print(mls)
